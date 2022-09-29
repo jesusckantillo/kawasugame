e567ba23d9055a5e35b90744e7973021191a5ce1
Jesus David Cantillo Guerrero <jesuscantillocode@gmail.com>
2022-09-29 00:51:48 -0500

    Version con obstaculos

--- src/GameSketch.java
+++ src/GameSketch.java
@@ -1,55 +1,49 @@
 import processing.core.PApplet;
 import processing.core.PImage;
+import java.util.*;
 import java.util.Random;
 
-public class GameSketch extends PApplet {
-
-    float maxc1= 250;
-    float minc1= 10;
-    float maxc2= 540;
-    float minc2 = 320;
+public  class GameSketch extends PApplet {
+    int cant = 8;
+    Obstaculos o = new Obstaculos(cant);
+    int  width = 600;
+    float xr;
+    obstaculo a,b,c,d,e,f,g;
     bus b1;
     bus b2;
     PImage bg;
     calle c1;
     calle c2;
-    obstaculo a,b,c,d,e,f,g,h,i,j;
 
     @Override
     public void settings() {
-        size(600, 600);
+        size(width, 600);
     }
 
 
    //Metodo generador de posicion inicial en x de los obstaculos
-    public float generator(float max,float min){
-        float x = (float) (Math.random() * (max+1 - min)) + min;
-        return x;
-    }
-
-
 
     @Override
     public void setup() {
+        frameRate(60);
         b1= new bus(width/4+width/2,height,40,80);
         b2= new bus(width/2-width/4,height,40,80);
-        bg= loadImage("background2.png");
+        bg= loadImage("background2.jpg");
         imageMode(CORNER);
         bg.resize((int) (width/2+b1.ancho), height*5);
         c1= new calle(-20,-1500,10);
         c2= new calle(width/2-20,-1500,10);
+        a = new obstaculo(-2000,15,0);
+        b = new obstaculo(-3000,15,1);
+        c = new obstaculo(-2800,15,3);
+        d = new obstaculo(-3700,15,4);
+        e = new obstaculo(-2600,15,5);
+        f= new obstaculo(-2400,15,6);
+        g = new obstaculo(-3300,15,7);
+
 
         //Objetos del tipo obstaculo
-        a= new obstaculo(this.generator(maxc1,minc1),-1000,19);
-        b= new obstaculo(this.generator(maxc1,minc1),-1500,19);
-        c= new obstaculo(this.generator(maxc1,minc1),-2000,19);
-        d= new obstaculo(this.generator(maxc1,minc1),-2500,19);
-        e= new obstaculo(this.generator(maxc1,minc1),-3000,19);
-        f = new obstaculo(this.generator(maxc2,minc2),-4000,19);
-        g= new obstaculo(this.generator(maxc2,minc2),-4500,19);
-        h= new obstaculo(this.generator(maxc2,minc2),-5000,19);
-        i= new obstaculo(this.generator(maxc2,minc2),-5500,19);
-        j= new obstaculo(this.generator(maxc2,minc2),-6000,19);
+
     }
 
     //manejar teclas presionadas
@@ -90,7 +84,7 @@ public class GameSketch extends PApplet {
     public void draw() {
         background(0); // reset del fondo
         //orden igual a capa
-
+        System.out.println(frameRate);
         c1.move();
         c1.display();
         c1.loop();
@@ -99,6 +93,7 @@ public class GameSketch extends PApplet {
         c2.display();
         c2.move();
         c2.loop();
+
 
         b1.display();
         b1.move();
@@ -112,6 +107,7 @@ public class GameSketch extends PApplet {
         b.display();
         b.move();
         b.loop();
+
         c.display();
         c.move();
         c.loop();
@@ -131,21 +127,6 @@ public class GameSketch extends PApplet {
         g.display();
         g.move();
         g.loop();
-
-        h.display();
-        h.move();
-        h.loop();
-
-        i.display();
-        i.move();
-        i.loop();
-
-        j.display();
-        j.move();
-        j.loop();
-
-
-
 
 
 
@@ -240,47 +221,57 @@ public class GameSketch extends PApplet {
     }
 
     //Clase del tipo Obstaculo
-    class obstaculo{
-        float ox,oy;
+    class obstaculo {
+        float ox, oy;
         float voy;
         float h;
+        ArrayList <Float> obsta = o.generador();
+
         //Constructor
-        obstaculo(float o_x,float o_y,float Voy) {
+        obstaculo( float o_y, float Voy,int pos) {
             oy = o_y;
             h = oy; //Guardo su posicion inicla en y para que vuelva a ella al momento de llegar a y =0.0
-            ox = o_x;
+            ox = obsta.get(pos);
             voy = Voy;
         }
+
         public void move(){
             oy = oy + voy;
         }
-        //Metodo loop
-        public void loop() {
-            if (oy == 600.0) {
-                oy = h;
-                float randomCalle =(float) Math.random();
-                if(randomCalle >0.5) {
-                    ox = (float) (Math.random() * (maxc1 + 1 - minc1)) + minc1;
-                }else{
-                    ox = (float) (Math.random() * (maxc2 + 1 - minc2)) + minc2;
-                }
 
-
-            }
-        }
-        public void display(){
-            fill(23);
+        public void  display(){
+            fill(255);
             rect(ox,oy,40,40);
         }
 
-
+        public void loop(){
+            if(oy>600){
+                System.out.println("PING");
+                oy = h;
+                ArrayList <Float> obsta2 = o.generador();
+                ox = obsta2.get((int) (Math.random() * cant) + 1);
+            }
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 
     public void run() {
         String[] processingArgs = {this.getClass().getName()};
         PApplet.runSketch(processingArgs, this);
     }
-
 
 
 }
---
+++ src/Obstaculos.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Obstaculos {
+    ArrayList<Float> obs = new ArrayList<Float>();
+    int numobstaculos;
+    Obstaculos(int obstaculos){
+        numobstaculos = obstaculos;
+    }
+    public ArrayList<Float> generador(){
+        float num;
+        for(int i=0;i<numobstaculos;i++){
+            do {
+                 num = (float) (Math.random() * 600) + 1;
+            }while(num<40 || num>560 ||(num>240 &num<320));
+            obs.add(num);
+        }
+        return(obs);
+    }
+}
